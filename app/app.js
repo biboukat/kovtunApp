d58b2075ea28fc73ed70370b07659761545f50a4
Alexander Kovtun <akovtun93@gmail.com>
2018-10-24 11:52:36 +0300

    added firebase SDK ios/android (#5)

--- app/app.js
+++ app/app.js
@@ -4,15 +4,11 @@ import { Text } from 'react-native';
 import { store } from '~/store/configureStore';
 import { Provider } from 'react-redux';
 import createStackNavigator from '~/screens';
-import firebase from 'firebase';
-import { firebaseInit } from '~/firebaseStore';
 import { isLoggedIn } from '~/selectors/auth';
 
 const InitialScreen = createStackNavigator(isLoggedIn(store.getState()));
 
 Text.defaultProps = { allowFontScaling: false };
-
-firebaseInit();
 
 class App extends Component {
   render() {
@@ -24,5 +20,4 @@ class App extends Component {
   }
 }
 
-export { firebase };
 export default App;
